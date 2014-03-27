5c3b37751a1fecc92ca5820c48eb0ad59f5608ba
Mads <mosenmeyer@hotmail.com>
2014-03-27 19:35:27 +0100

    ,
    HotelGui

---
+++ HotelGui/src/domain/Controller.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author Mads
+ */
+public class Controller
+{
+    
+}
