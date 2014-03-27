3154eecbb4e86f9e8ab85c7dd9888e341157d50a
ilicitdragons <Ilici@10.18.138.32>
2014-03-27 11:39:16 +0100

    room class update : constructor, setters and getters

--- Program/Java/Casablanca/src/domain/Room.java
+++ Program/Java/Casablanca/src/domain/Room.java
@@ -16,4 +16,44 @@ public class Room {
     private int RoomBookingId;
     private String Name;
 
+    public Room(int Id, int RoomTypeId, int RoomBookingId, String Name) {
+        this.Id = Id;
+        this.RoomTypeId = RoomTypeId;
+        this.RoomBookingId = RoomBookingId;
+        this.Name = Name;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public int getRoomTypeId() {
+        return RoomTypeId;
+    }
+
+    public int getRoomBookingId() {
+        return RoomBookingId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public void setRoomTypeId(int RoomTypeId) {
+        this.RoomTypeId = RoomTypeId;
+    }
+
+    public void setRoomBookingId(int RoomBookingId) {
+        this.RoomBookingId = RoomBookingId;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    
+
 }
